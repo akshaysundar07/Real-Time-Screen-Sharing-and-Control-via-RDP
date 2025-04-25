d1c590434dfcf12874db932a08cf782aafe35736
Akshay Sundar T S <cb.sc.u4aie23004@cb.students.amrita.edu>
2025-04-25 14:27:05 +0530

    Create Start.java

---
+++ Server/Start.java
@@ -0,0 +1,11 @@
+package src.Server;
+
+
+public class Start {
+    public static void main(String[] args) {
+        SetPassword frame1 = new SetPassword();
+        frame1.setSize(300, 80);
+        frame1.setLocation(500, 300);
+        frame1.setVisible(true); 
+    }
+}
